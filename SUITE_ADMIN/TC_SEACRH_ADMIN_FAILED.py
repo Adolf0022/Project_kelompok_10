299f073c6bd79951494090cd6b4da6b71e1d5f57
Adolf0022 <120384008+Adolf0022@users.noreply.github.com>
2022-12-19 10:23:32 +0700

    Add files via upload

---
+++ SUITE_ADMIN/TC_SEACRH_ADMIN_FAILED.py
@@ -0,0 +1,74 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+
+class test_a_admin_search_failed(unittest.TestCase):
+    def setUp(self): 
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        
+    def test_add_a_userorle(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com") #OPEN BROWSER
+        time.sleep(5)
+        driver.find_element(By.NAME, "username").send_keys("Admin") #INPUT USERNAME
+        time.sleep(2)
+        driver.find_element(By.NAME, "password").send_keys("admin123") #INPUT PASSWORD
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']").click()    #CLICK BUTTON LOGIN
+        time.sleep(2)  
+        driver.find_element(By.XPATH, "//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/dashboard/index']/span[.='Dashboard']" ) #VIEW DASHBOARD
+        time.sleep(2)                               
+        driver.find_element(By.XPATH, "//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/admin/viewAdminModule']" ).click() #VIEW ADMIN
+        time.sleep(4)
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input').send_keys("Odis Adalwin") #INPUT USERNAME
+        time.sleep(2)
+     
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[2]/i').click() #dropdown userrole
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/span').click() #choose Admin
+        time.sleep(2)
+        
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[4]/div/div[2]/div/div/div[2]/i').click() #dropdown STATUS
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[4]/div/div[2]/div/div[2]/div[2]').click() #choose enabled
+        time.sleep(2)
+        
+        
+
+
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]' ).click() #BUTTON SAerch
+        time.sleep(5)
+
+    def test_add_b_userorle(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com") #OPEN BROWSER
+        time.sleep(5)
+        driver.find_element(By.NAME, "username").send_keys("Admin") #INPUT USERNAME
+        time.sleep(2)
+        driver.find_element(By.NAME, "password").send_keys("admin123") #INPUT PASSWORD
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']").click()    #CLICK BUTTON LOGIN
+        time.sleep(2)  
+        driver.find_element(By.XPATH, "//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/dashboard/index']/span[.='Dashboard']" ) #VIEW DASHBOARD
+        time.sleep(2)                               
+        driver.find_element(By.XPATH, "//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']//ul[@class='oxd-main-menu']//a[@href='/web/index.php/admin/viewAdminModule']" ).click() #VIEW ADMIN
+        time.sleep(4)
+     
+        
+    
+
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]' ).click() #BUTTON SAerch
+        time.sleep(5)
+
+
+
+
+        def tearDown(self): 
+            self.driver.close()
+        
+if __name__ == "__main__":
+    unittest.main()
